d6231629b6d47c510083ef19d279fc4b85baa5b4
Shrijana Maharjan <120613068+Shrijana1029@users.noreply.github.com>
2024-12-13 16:55:18 +0545

    shree: manage profile page

--- lib/screens/front_page.dart
+++ lib/screens/front_page.dart
@@ -1,10 +1,9 @@
 import 'package:fitnessapp/screens/breakfast_page.dart';
-import 'package:fitnessapp/screens/food_details.dart';
 import 'package:fitnessapp/screens/home.dart';
-// import 'package:fitnessapp/screens/login_page.dart';
+import 'package:fitnessapp/screens/manage_profle.dart';
 import 'package:fitnessapp/screens/profile.dart';
-// import 'package:fitnessapp/screens/signup_page.dart';
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 
 class FrontPage extends StatefulWidget {
   const FrontPage({super.key});
@@ -14,113 +13,69 @@ class FrontPage extends StatefulWidget {
 }
 
 class _FrontPageState extends State<FrontPage> {
-  int _selectedindex = 0;
+  int selectedIndex = 0;
 
-  void _tappedItem(int index) {
+  void _handleSelectedIndex(int index) {
     setState(() {
-      _selectedindex = index;
+      selectedIndex = index;
     });
   }
 
-  // make list for display text
-  final List<Widget> _pages = [
-    const Home(),
-    const BreakFast(),
-    // scrollbottom(context)
-    // const Profile()
-    const Home()
-  ];
+  final PersistentTabController _controller =
+      PersistentTabController(initialIndex: 0);
+  List<Widget> _buildScreens() {
+    return [
+      Home(),
+      BreakFast(),
+      Profile(),
+      ManageProfle()
+      // Profile(),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarItems() {
+    return [
+      PersistentBottomNavBarItem(
+          icon: Icon(Icons.home),
+          title: "Home",
+          inactiveColorPrimary: Colors.grey.shade600,
+          activeColorPrimary: Colors.purple),
+      PersistentBottomNavBarItem(
+          icon: Icon(Icons.menu_book_sharp),
+          title: "Dashboard",
+          inactiveColorPrimary: Colors.grey.shade600,
+          activeColorPrimary: Colors.purple),
+      PersistentBottomNavBarItem(
+          icon: Icon(Icons.calendar_month_rounded),
+          inactiveColorPrimary: Colors.grey,
+          title: "Home",
+          activeColorPrimary: Colors.purple),
+      PersistentBottomNavBarItem(
+          icon: Icon(Icons.more_horiz),
+          title: "Home",
+          inactiveColorPrimary: Colors.grey.shade600,
+          activeColorPrimary: Colors.purple),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 184, 216, 201),
-      ///////////APP BAR CONTEXT//////////
-      appBar: AppBar(
-        leading: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ClipOval(
-            child: Image.asset(
-              'assets/img/user.jpg',
-              height: 40,
-              width: 40,
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.messenger)),
-        ],
-        backgroundColor: const Color.fromARGB(255, 184, 216, 201),
-      ),
-      body: _pages[_selectedindex],
+    return PersistentTabView(
+      backgroundColor: const Color.fromARGB(255, 184, 216, 201), context,
+      screens: _buildScreens(),
+      controller: _controller,
+      items: _navBarItems(),
+      confineToSafeArea: true,
+      handleAndroidBackButtonPress: true,
+      resizeToAvoidBottomInset: true,
+      stateManagement: true,
+      decoration: NavBarDecoration(borderRadius: BorderRadius.circular(0.5)),
+      navBarStyle: NavBarStyle.simple,
 
-      //////////BOTTOM NAVIGATION BAR////////
-      bottomNavigationBar: Container(
-        height: 90,
-        decoration: const BoxDecoration(
-            color: Color.fromARGB(255, 184, 216, 201),
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(40),
-              topRight: Radius.circular(50),
-            )),
-        child: BottomNavigationBar(
-          onTap: _tappedItem,
-          backgroundColor: Colors.transparent,
-
-          currentIndex: _selectedindex,
-          selectedItemColor: const Color.fromARGB(255, 233, 218, 115),
-          unselectedItemColor: Colors.black,
-          showSelectedLabels: true, //display labels for selected items
-          // showUnselectedLabels: false, //hides labels for unselected items
-          type: BottomNavigationBarType.fixed,
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home), label: 'sDashboard'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.menu_book_sharp), label: 'Diary'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.calendar_month_rounded), label: 'Plans'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.more_horiz), label: 'Profile'),
-          ],
-        ),
-      ),
+      // stores the current index of navbaritems
+      onItemSelected: (int selectedIndex) {
+        _handleSelectedIndex(selectedIndex);
+      },
     );
   }
-}
-
-Future scrollbottom(BuildContext context) {
-  // return showModalBottomSheet(
-  //     context: context,
-  //     builder: (context) {
-  //       return StatefulBuilder(
-  //           builder: (BuildContext context, StateSetter setState) {
-  //         return Container();
-  //       });
-  //     }
-  //     );
-  return showModalBottomSheet(
-    context: context,
-    backgroundColor: Colors.white,
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
-    ),
-    builder: (context) {
-      return Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text('This is a Modal Bottom Sheet'),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text('Close'),
-            ),
-          ],
-        ),
-      );
-    },
-  );
 }
--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,3 +1,4 @@
+import 'package:fitnessapp/screens/manage_profle.dart';
 import 'package:flutter/material.dart';
 import 'package:pedometer/pedometer.dart';
 
@@ -11,6 +12,30 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: InkWell(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ManageProfle()));
+            },
+            child: ClipOval(
+              child: Image.asset(
+                'assets/img/user.jpg',
+                height: 40,
+                width: 40,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.messenger)),
+        ],
+        backgroundColor: const Color.fromARGB(255, 184, 216, 201),
+      ),
+    );
   }
 }
